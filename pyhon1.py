2fe0d0112c0fa8e2940ce738002f9202e1dc5ce1
JiOh Lee <jiohlee2004@naver.com>
2026-04-29 10:29:55 +0900

    add: 사칙연산

---
+++ pyhon1.py
@@ -0,0 +1,9 @@
+### 사칙연산
+num1 = int(input("num1:"))
+num2 = int(input("num2:"))
+op = input("op:")
+
+if op == '+': print(num1+num2)
+elif op == '-': print(num1-num2)
+elif op == '*': print(num1*num2)
+elif op == '/': print(num1/num2)
